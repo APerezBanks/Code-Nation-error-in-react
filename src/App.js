439ea91ec5e12d2d1b70146ee2b4a7526d637093
Alejandro Perez Banks <alejandroperezbanks@MacBook-Pro-de-Alejandro.local>
2022-02-14 15:27:59 +0000

    error

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react'
+const App = () => {
+  const [advice, setAdvice] = useState("")
+  const [error, setError] = useState({
+    error: false,
+    message: ""
+  })
+  const collect = async () => {
+    try {
+      const response = await fetch("https://api.adviceslip.com/advice")
+      console.log(response) 
+      if(response.status !== 200){
+        throw new Error("oops")
+      }
+      const data = await response.json()
+      setAdvice(data.slip)
+    } catch (error) {
+      setError({ error: true, message: error.message })
+    }
+  }
 
-function App() {
+  useEffect(() => {
+    collect()
+    console.log("oop")
+  }, [])
+
+  if(error.error){
+    return <h1>an error has occured: {error.message} </h1>
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>advice: {advice.advice}</h1>
+      <button onClick={collect}>fetch</button>
     </div>
-  );
+  )
 }
-
-export default App;
+export default App
